97f99805e26659b084df946fca92ea0172401c93
Hritik Anand <realhga@gmail.com:--global user.email>
2024-03-26 13:08:03 +0530

    Dark mode functionality is added

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Button, Navbar, TextInput, Dropdown, Avatar } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';  
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 import {HiLogout, HiUser } from "react-icons/hi";
 function Header() {
     const path = useLocation().pathname;  //yeh batane ke liye agr home page pr h toh home button menu me highlighted rahe 
     const { currentUser } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const {theme} = useSelector((state)=> state.theme);
   return (
     <Navbar className='border-b-2'>   
       <Link
@@ -33,8 +36,13 @@ function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+      <Button
+          className='w-12 h-10 hidden sm:inline'
+          color='gray'
+          pill
+          onClick={() => dispatch(toggleTheme())}
+        >
+          {theme === 'light' ? <FaMoon /> : <FaSun />}
         </Button>
           {currentUser ? (
           <Dropdown
