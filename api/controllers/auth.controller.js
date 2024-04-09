968ab977ba0c075fa809b5632f1c869d88b13b47
Hritik Anand <realhga@gmail.com:--global user.email>
2024-04-09 19:08:52 +0530

    Create a post page UI is added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,7 +51,7 @@ export const signin = async (req,res,next) =>{
       return next(errorHandler(400, 'Wrong Credentials'));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {expiresIn: 3600});
+    const token = jwt.sign({ id: validUser._id ,isAdmin: validUser.isAdmin}, process.env.JWT_SECRET, {expiresIn: 3600});
 
     const { password: pass, ...rest } = validUser._doc;
 
@@ -73,7 +73,7 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -95,7 +95,7 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, TextInput,Modal, ModalBody} from 'flowbite-react';
 import { useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import {
     getDownloadURL,
     getStorage,
@@ -23,7 +24,7 @@ import {
   import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
 export default function DashProfile() {
-    const { currentUser, error } = useSelector((state) => state.user);
+    const { currentUser, error, loading } = useSelector((state) => state.user);
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl] = useState(null);
     const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
@@ -215,9 +216,20 @@ export default function DashProfile() {
             onChange={handleChange}
           />
           <TextInput type='password' id='password' placeholder='password' onChange={handleChange}/>
-          <Button type='submit' gradientDuoTone='purpleToPink' outline>
-              Update
+          <Button type='submit' gradientDuoTone='purpleToPink' outline disabled={loading || imageFileUploading}>
+          {loading ? 'Loading...' : 'Update'}
           </Button>
+          {currentUser.isAdmin && (
+          <Link to={'/create-post'}>
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+              Create a post
+            </Button>
+          </Link>
+        )}
         </form>
         <div className="text-red-500 flex justify-between mt-5">
           <span onClick={() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
