f9d245dbd90597eaea928978ca9df9a1063b9858
Hritik Anand <realhga@gmail.com:--global user.email>
2024-05-25 11:41:23 +0530

    Comment to the admin dashboard is added

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,5 +1,5 @@
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiLogout, HiDocumentText, HiOutlineUserGroup } from 'react-icons/hi';
+import { HiUser, HiLogout, HiDocumentText, HiOutlineUserGroup, HiAnnotation, } from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -59,6 +59,7 @@ export default function DashSidebar() {
             </Link>
           )}
           {currentUser.isAdmin && (
+            <>
             <Link to='/dashboard?tab=users'>
               <Sidebar.Item
                 active={tab === 'users'}
@@ -68,6 +69,16 @@ export default function DashSidebar() {
                 Users
               </Sidebar.Item>
             </Link>
+            <Link to='/dashboard?tab=comments'>
+            <Sidebar.Item
+              active={tab === 'comments'}
+              icon={HiAnnotation}
+              as='div'
+            >
+              Comments
+            </Sidebar.Item>
+          </Link>
+        </>
           )}
           <Sidebar.Item icon={HiLogout} className='cursor-pointer' onClick={handleSignout}>
             Sign Out
