353ce385b1f2dd327a0405131b58c15fed11673a
Hritik Anand <realhga@gmail.com:--global user.email>
2024-07-16 23:35:41 +0530

    Search Page is completed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Button, Navbar, TextInput, Dropdown, Avatar } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';  
+import { Link, useLocation, useNavigate } from 'react-router-dom';  
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import {HiLogout, HiUser } from "react-icons/hi";
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 function Header() {
     const path = useLocation().pathname;  //yeh batane ke liye agr home page pr h toh home button menu me highlighted rahe 
     const { currentUser } = useSelector((state) => state.user);
+    const location = useLocation();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const {theme} = useSelector((state)=> state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl);
+      }
+    }, [location.search]);
+
     const handleSignout = async () => {
       try {
         const res = await fetch('/api/user/signout', {
@@ -27,6 +40,15 @@ function Header() {
         console.log(error.message);
       }
     };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      const urlParams = new URLSearchParams(location.search);
+      urlParams.set('searchTerm', searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
+    };
+
   return (
     <Navbar className='border-b-2'>   
       <Link
@@ -40,12 +62,14 @@ function Header() {
           BBY
         </span>
       </Link>
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='green' pill>
