f161bfb82ceedf34f5834aeb9dc7a48eb7808279
Hritik Anand <realhga@gmail.com:--global user.email>
2024-08-12 10:31:45 +0530

    Slight changes 1

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
-import DashComments from '../components/Dashcomments';
+import DashComments from '../components/DashComments';
 import DashboardComp from '../components/DashboardComp';
 function Dashboard() {
   const location = useLocation();
